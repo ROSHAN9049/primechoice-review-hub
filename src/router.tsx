0059a33f019f74357eefec6e892caee356c0a3f0
Roshan Nattuji Shambharkar <roshanshambharkar62@gmail.com>
2026-08-12 08:04:00 +0530

    Refactor router setup and import statements

--- src/router.tsx
+++ src/router.tsx
@@ -1,27 +1,7 @@
 // src/router.tsx
-import { createRouter, createRootRoute, createRoute } from "@emstrack/react-start";
+import { createRouter } from "@emstrack/react-start";
+import { routeTree } from "./routeTree.gen";
 
-// 1. ROOT LAYOUT (Wrapper for all pages)
-const rootRoute = createRootRoute({
-  component: () => import('./routes/_root').then(m => m.Route.component),
+export const router = createRouter({
+  routeTree,
 });
-
-// 2. HOMEPAGE (/)
-const indexRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: '/',
-  component: () => import('./routes/index').then(m => m.Route.component),
-});
-
-// 3. ADMIN PANEL (/admin) - Ab yeh 100% kaam karega
-const adminRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: '/admin',
-  component: () => import('./routes/admin').then(m => m.Route.component),
-});
-
-// 4. Saari routes ko combine karo
-const routeTree = rootRoute.addChildren([indexRoute, adminRoute]);
-
-// 5. Router export karo
-export const router = createRouter({ routeTree });
